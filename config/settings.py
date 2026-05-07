b2fef74716098cb08dd8867db73e9f64fd3da410
Dioces123 <cls.3hax@gmail.com>
2026-05-07 07:42:37 +0000

    Switch to PostgreSQL for production

--- config/settings.py
+++ config/settings.py
@@ -58,11 +58,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
